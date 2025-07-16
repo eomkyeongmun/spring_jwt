090e02bf6dc77694100738f0ee1df8e426066c70
엄경문 <eomkyeongmun@naver.com>
2025-07-16 14:12:35 +0900

    feat:jwt 방식 구현

---
+++ src/main/java/com/example/test_jwt/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.test_jwt.repository;
+
+import com.example.test_jwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByUsername(String username);
+}
